7540ffa6b8f8fd4c121e87b6950c7a90030c0075
Harutyun Gzraryan <hgzraryan@yandex.ru>
2022-05-30 18:39:01 +0400

    First commit_30.05.2022

---
+++ app.js
@@ -0,0 +1,172 @@
+require("dotenv").config();
+require("./config/database").connect();
+
+
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const User = require("./model/user");
+const auth = require("./middleware/auth");
+
+const app = express();
+
+const bodyParser = require("body-parser");
+
+const oracledb = require('oracledb');
+const mypw = "TCTDBS";  // set mypw to the hr schema password
+
+
+app.use(express.json({ limit: "50mb" }));
+
+app.post("/register", async (req, res) => {
+  try {
+    // Get user input
+    const { first_name, last_name, email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password && first_name && last_name)) {
+      res.status(400).send("All input is required");
+    }
+
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.get("/welcome", auth, (req, res) => {
+ 
+  
+  
+  
+  async function run() {
+
+	  let connection;
+
+	  try {
+		connection = await oracledb.getConnection( {
+		  user          : "TCTDBS",
+		  password      : mypw,
+		  connectString : "onlinedbn-scan/online"
+		});
+
+		const result = await connection.execute(
+		  `SELECT count(*)
+		   FROM pgorders
+		   WHERE merchantid = :id`,
+		  [29004486],  // bind value for :id
+		);
+		//console.log(result.rows);
+		return result;
+
+	  } catch (err) {
+		console.error(err);
+	  } finally {
+		if (connection) {
+		  try {
+			await connection.close();
+		  } catch (err) {
+			console.error(err);
+		  }
+		}
+	  }
+	}
+
+
+	get = run();
+	
+	res.status(200).json({
+		
+		success: get
+		
+	});
+	
+
+	
+	//run();
+  //console.log(get);
+   //res.status(200).send(get);
+  
+  
+  
+});
+
+// This should be the last route else any after it won't work
+app.use("*", (req, res) => {
+  res.status(404).json({
+    success: "false",
+    message: "Page not found",
+    error: {
+      statusCode: 404,
+      message: "You reached a route that is not defined on this server",
+    },
+  });
+});
+
+module.exports = app;
